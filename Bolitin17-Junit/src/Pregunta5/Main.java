03d8473bb9caae4a00e2b2bb9edbcb16523d0f2c
eloydeira <eloydeira@gmail.com>
2017-05-04 10:17:44 +0200

    Este e o terceiro commit

--- Bolitin17-Junit/src/Pregunta5/Main.java
+++ Bolitin17-Junit/src/Pregunta5/Main.java
@@ -8,7 +8,8 @@ public class Main {
 		
 		xeo.perimetroCadrado(2);
 		xeo.areaCadrado(2);
-		xeo.teoremaPitagoras(5,10);
+		xeo.teoremaPitagoras(8,10);
+		xeo.teoremaPitagoras2(10, 8);
 		
 		
 	}
--- Bolitin17-Junit/src/Pregunta5/Xeometria.java
+++ Bolitin17-Junit/src/Pregunta5/Xeometria.java
@@ -7,23 +7,30 @@ public class Xeometria {
 		
 	
 	public int perimetroCadrado(int lado){ 
-		lado =2;
+		
 		System.out.println("Perimetro Cuadrado: " + lado*4);
 		return lado;
 	}	
 	
 		
 	public int areaCadrado(int lado) {
-		lado =2;
+		
 		System.out.println("Area Cadrado: " + lado*lado);
 		return lado;
 	}
 	public double teoremaPitagoras(int catetoA, int catetoB) {
 		double hipotenusaC;
 		
-		System.out.println("teorema de pitágoras ");
+		
+		System.out.println("Teorema de pitágoras: " + (hipotenusaC=(catetoA*catetoA)+(catetoB*catetoB)) );
 		return hipotenusaC= Math.sqrt((catetoA*catetoA) + (catetoB*catetoB));
 		 
+	}
+	public double teoremaPitagoras2(double hipotenusa, double catetoA){
+		double catetoB;
+		
+		System.out.println("Teorema de pitágoras: " + (catetoB=(hipotenusa*hipotenusa)-(catetoA*catetoA)));
+		return catetoB=Math.sqrt(catetoB=(hipotenusa*hipotenusa)-(catetoA*catetoA));
 	}
 	
 }
