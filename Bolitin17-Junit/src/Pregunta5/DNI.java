23f6a64e4d8a4fb746e7f9cf15a502634b923231
eloydeira <eloydeira@gmail.com>
2017-05-08 12:28:17 +0200

    Cuarto commit

---
+++ Bolitin17-Junit/src/Pregunta5/DNI.java
@@ -0,0 +1,28 @@
+package Pregunta5;
+
+public class DNI {
+	private char [] dnilet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
+	
+	
+	public boolean eValido(String numeroLetra) {
+		String letraMayuscula = "";
+		if(dni.length() !=9 ||Character.isLetter(this.dni.charAt(8))== false){
+			return false;
+		
+		
+		
+		}
+
+	}
+	public int calculaLetra(String numero) {
+		
+		
+	}
+	public boolean eValido(ArrayList<Integer>numeros, char letra){
+		
+	}
+	public int calculaLetra(ArrayList<Integer>numeros) {
+		
+	}
+
+}
