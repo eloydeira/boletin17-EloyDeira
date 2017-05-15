ac17b00d015aa2fbe736c97ddbf2abaa28201fdc
eloydeira <eloydeira@gmail.com>
2017-05-15 13:45:04 +0200

    Septimo commit

--- Bolitin17-Junit/src/Pregunta5/DNI.java
+++ Bolitin17-Junit/src/Pregunta5/DNI.java
@@ -1,69 +1,64 @@
 package Pregunta5;
-import java.util.Scanner;
+
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import java.util.ArrayList;
 public class DNI {
-	/*private char[] dnilet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};*/
+	private char[] dnilet = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q',
+			'V', 'H', 'L', 'C', 'K', 'E'};
 	
 	
-	public boolean eValido(String numeroLetra) {
-		 boolean correcto = false;
-
-		    Pattern pattern = Pattern.compile("(\\d{1,8})([TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke])");
-
-		    Matcher matcher = pattern.matcher(numeroLetra);
-
-		    if (matcher.matches()) {
-
-		        String letra = matcher.group(2);
-
-		        String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
-
-		        int index = Integer.parseInt(matcher.group(1));
-
-		        index = index % 23;
-
-		        String reference = letras.substring(index, index + 1);
-
-
-
-		        if (reference.equalsIgnoreCase(letra)) {
-
-		            correcto = true;
-
-		        } else {
-
-		            correcto = false;
-
-		        }
-
-		    } else {
-
-		        correcto = false;
-		        
-
-		    }
-
-		    return correcto;
-
+	public boolean eValido(String numeroLetra) {//non esta ben, hai que modificalo
+		
+		String dni = null;
+				
+		
+		//comprobar que a longitud sea 9
+		if(dni.length() ==9) {
+			return true;
+		
+				//comprobar que os 8 primeiros digitos sean numeros
+				for(int i=0; i<8; i++){
+					if(!Character.isDigit(dni.charAt(i))){
+						return true;
+					
+						//comprobar que o 9º digito e unha letra
+						if(!Character.isLetter(dni.charAt(8))){
+							return true;
+					}
+					else 
+						return false;
+			}
+					else 
+						return false;
+				}
+		
+			return false;
+		
+		
 		}
 	
-public int calculaLetra(int dni) {
-	String dnilet ="TRWAGMYFPDXBNJZSQVHLCKE";
-	int modulo = dni % 23;
-	char letra = dnilet.charAt(modulo);
-	return letra;
-	
+public int calculaLetra(String numero) {//mal, volver mirar
+	int modulo;
+
+	if (numero.length() == 8) {
+		for (int i = 0; i < 8; i++)
+			if (numero.charAt(i) < '0' || numero.charAt(i) > '9')
+				return -1;
+	} else
+		return -1;
+	int numEntero = Integer.parseInt(numero);
+	modulo = numEntero % 23;
+return modulo;
 }
 		
-	/*	public boolean eValido(ArrayList<Integer>numeros, char letra){
+	//	public boolean eValido(ArrayList<Integer>numeros, char letra){
 			
-		}
-		public int calculaLetra(ArrayList<Integer>numeros) {
+		//}
+		//public int calculaLetra(ArrayList<Integer>numeros) {
 			
-		}*/	
+	//	}
 	
 
 }
